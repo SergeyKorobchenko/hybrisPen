ac87d73acaaa12966ae43025f0cd7f9063e2f13d
lenar <Sana2104>
2017-10-02 16:31:10 +0300

    #BRIDGEX-10 Brand page template

---
+++ hybris/bin/custom/pentland/pentlandcore/src/com/bridgex/core/url/impl/PentlandCategoryModelUrlResolver.java
@@ -0,0 +1,20 @@
+package com.bridgex.core.url.impl;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import de.hybris.platform.category.model.CategoryModel;
+import de.hybris.platform.commerceservices.url.impl.DefaultCategoryModelUrlResolver;
+
+public class PentlandCategoryModelUrlResolver extends DefaultCategoryModelUrlResolver {
+
+  @Override
+  protected String resolveInternal(final CategoryModel source) {
+
+    boolean isBrandCategory = CollectionUtils.isEmpty(source.getSupercategories());
+    if (isBrandCategory) {
+      return StringUtils.isNotBlank(source.getName()) ? source.getName() : source.getCode();
+    }
+    return super.resolveInternal(source);
+  }
+}
