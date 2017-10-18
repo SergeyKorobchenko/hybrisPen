913e52602f53c949188de37a16c7cec9d023abb7
Kate Agievich <ekaterina.agievich@masterdata.ru>
2017-10-18 16:33:51 +0300

    #BRIDGEX-23 filter out discontinued and unapproved products at indexing time

---
+++ hybris/bin/custom/pentland/pentlandcore/src/com/bridgex/core/search/solrfacetsearch/provider/PentlandFsQueryParameterProvider.java
@@ -0,0 +1,25 @@
+package com.bridgex.core.search.solrfacetsearch.provider;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.bridgex.core.enums.DiscontinuedStatus;
+
+import de.hybris.platform.catalog.enums.ArticleApprovalStatus;
+import de.hybris.platform.solrfacetsearch.provider.ParameterProvider;
+
+/**
+ * @author Created by ekaterina.agievich@bridge-x.com on 10/18/2017.
+ */
+public class PentlandFsQueryParameterProvider implements ParameterProvider{
+  @Override
+  public Map<String, Object> createParameters() {
+    HashMap<String, Object> parameters = new HashMap<>();
+
+    parameters.put("discontinuedStates", Arrays.asList(DiscontinuedStatus.D03, DiscontinuedStatus.D04, DiscontinuedStatus.D05));
+    parameters.put("approvalStatus", ArticleApprovalStatus.APPROVED);
+
+    return parameters;
+  }
+}
