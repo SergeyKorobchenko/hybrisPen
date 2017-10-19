f4ed42673aa2812d05df307ad823f0b65439bb17
Konstantin Pavlyukov <Konstantin.Palvyukov@masterdata.ru>
2017-10-19 16:08:12 +0100

    BRIDGEX-20 separator changed

--- hybris/bin/custom/pentland/pentlandcore/src/com/bridgex/core/services/PentlandB2BUnitService.java
+++ hybris/bin/custom/pentland/pentlandcore/src/com/bridgex/core/services/PentlandB2BUnitService.java
@@ -3,7 +3,7 @@ package com.bridgex.core.services;
 import de.hybris.platform.b2b.model.B2BUnitModel;
 
 /**
- * @author Created by konstantin.pavlyukov@masterdata.ru on 10/19/2017.
+ * @author Created by konstantin.pavlyukov@bridge-x.com on 10/19/2017.
  */
 public interface PentlandB2BUnitService {
   B2BUnitModel getUnitBySapID(String sapID);
