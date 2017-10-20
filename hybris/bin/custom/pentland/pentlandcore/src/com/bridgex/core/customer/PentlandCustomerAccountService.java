16f9cbc54923b1d56b47567a85cb831d2840431a
dmitry.konovalov <dmitry.konovalov@masterdata.ru>
2017-10-20 14:23:00 +0300

    #BRIDGEX-21 updated order history

---
+++ hybris/bin/custom/pentland/pentlandcore/src/com/bridgex/core/customer/PentlandCustomerAccountService.java
@@ -0,0 +1,18 @@
+package com.bridgex.core.customer;
+
+import de.hybris.platform.commerceservices.customer.CustomerAccountService;
+import de.hybris.platform.commerceservices.search.pagedata.PageableData;
+import de.hybris.platform.commerceservices.search.pagedata.SearchPageData;
+import de.hybris.platform.core.enums.OrderStatus;
+import de.hybris.platform.core.model.order.OrderModel;
+import de.hybris.platform.core.model.user.CustomerModel;
+import de.hybris.platform.store.BaseStoreModel;
+
+/**
+ * Created by dmitry.konovalov@masterdata.ru on 18.10.2017.
+ */
+public interface PentlandCustomerAccountService extends CustomerAccountService {
+
+  SearchPageData<OrderModel> getB2BOrderList(final CustomerModel customerModel, final BaseStoreModel store, final OrderStatus[] status, final PageableData pageableData);
+
+}
