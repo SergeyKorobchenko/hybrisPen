15f1a044041144fce5ea9309d3f3f720b14820a9
Konstantin Pavlyukov <Konstantin.Palvyukov@masterdata.ru>
2017-10-30 17:39:01 +0300

    BRIDGEX-30 service mock added

--- hybris/bin/custom/pentland/erpintegration/src/com/bridgex/integration/service/impl/OrderSimulateServiceImpl.java
+++ hybris/bin/custom/pentland/erpintegration/src/com/bridgex/integration/service/impl/OrderSimulateServiceImpl.java
@@ -1,10 +1,13 @@
 package com.bridgex.integration.service.impl;
 
+import com.bridgex.integration.domain.MultiBrandCartDto;
+import com.bridgex.integration.domain.MultiBrandCartResponse;
+
 /**
  * @author Created by konstantin.pavlyukov on 10/26/2017.
  */
 
-public class OrderSimulateServiceImpl<MultiBrandCartDto, MultiBrandCartResponse> extends AbstractIntegrationService<MultiBrandCartDto, MultiBrandCartResponse> {
+public class OrderSimulateServiceImpl extends AbstractIntegrationService<MultiBrandCartDto, MultiBrandCartResponse> {
 
   @Override
   public String getServiceName() {
