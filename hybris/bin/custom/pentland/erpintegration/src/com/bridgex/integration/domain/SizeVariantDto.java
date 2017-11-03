2d92fe31691d88afe7a1115ecb82a3887ee356fe
Mikhail Goncharenko <mikhail.goncharenko@masterdata.ru>
2017-11-03 19:24:08 +0300

    BRIDGEX-39 Minor changes  in DTO definitions

--- hybris/bin/custom/pentland/erpintegration/src/com/bridgex/integration/domain/SizeVariantDto.java
+++ hybris/bin/custom/pentland/erpintegration/src/com/bridgex/integration/domain/SizeVariantDto.java
@@ -1,7 +1,9 @@
 package com.bridgex.integration.domain;
 
+import java.util.Date;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
@@ -20,8 +22,15 @@ public class SizeVariantDto {
   @JsonProperty("TOTAL_QUANTITY")
   private String totalQuantity;
 
-  @JsonProperty("ET_SHIPMENTS")
-  private List<ShipmentDto> shipments;
+  @JsonProperty("E_SHIP_DATE")
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+  private Date shipDate;
+
+  @JsonProperty("E_SHIP_AMOUNT")
+  private String shipQty;
+
+  @JsonProperty("E_SHIP_STATUS")
+  private String shipStatus;
 
   public String getLineNumber() {
     return lineNumber;
@@ -47,11 +56,27 @@ public class SizeVariantDto {
     this.totalQuantity = totalQuantity;
   }
 
-  public List<ShipmentDto> getShipments() {
-    return shipments;
+  public Date getShipDate() {
+    return shipDate;
   }
 
-  public void setShipments(List<ShipmentDto> shipments) {
-    this.shipments = shipments;
+  public void setShipDate(Date shipDate) {
+    this.shipDate = shipDate;
+  }
+
+  public String getShipQty() {
+    return shipQty;
+  }
+
+  public void setShipQty(String shipQty) {
+    this.shipQty = shipQty;
+  }
+
+  public String getShipStatus() {
+    return shipStatus;
+  }
+
+  public void setShipStatus(String shipStatus) {
+    this.shipStatus = shipStatus;
   }
 }
