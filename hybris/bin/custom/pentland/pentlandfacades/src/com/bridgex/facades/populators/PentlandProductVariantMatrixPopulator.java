fa1f97b4778b1d130d807ad88cd5806172b24f98
Dmitry Konovalov <dmitry.konovalov@masterdata.ru>
2017-11-07 12:09:38 +0300

    #BRIDGEX-40

--- hybris/bin/custom/pentland/pentlandfacades/src/com/bridgex/facades/populators/PentlandProductVariantMatrixPopulator.java
+++ hybris/bin/custom/pentland/pentlandfacades/src/com/bridgex/facades/populators/PentlandProductVariantMatrixPopulator.java
@@ -1,5 +1,6 @@
 package com.bridgex.facades.populators;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 
@@ -25,27 +26,22 @@ public class PentlandProductVariantMatrixPopulator<SOURCE extends ProductModel, 
     final Collection<VariantProductModel> variants = productModel.getVariants();
     productData.setMultidimensional(Boolean.valueOf(CollectionUtils.isNotEmpty(variants)));
     ProductModel baseProductModel = getBaseProduct(productModel);
-    VariantMatrixElementData rootElementData = new VariantMatrixElementData();
-
+    VariantMatrixElementData rootElementData = createNode(baseProductModel);
 
     for (VariantProductModel styleLevelVariantModel : baseProductModel.getVariants()) {
       ApparelStyleVariantProductModel styleVariantProductModel = (ApparelStyleVariantProductModel) styleLevelVariantModel;
-      VariantMatrixElementData variantElementData = new VariantMatrixElementData();
-
+      VariantMatrixElementData styleElementData = createNode(styleVariantProductModel);
+      rootElementData.getElements().add(styleElementData);
 
       for (VariantProductModel sizeLevelVariantModel : styleVariantProductModel.getVariants()) {
         ApparelSizeVariantProductModel sizeVariantProductModel = (ApparelSizeVariantProductModel) sizeLevelVariantModel;
-
-        VariantMatrixElementData matrixElementData = new VariantMatrixElementData();
-        VariantOptionData variantOptionData = newVariantOptionData();
-        populateVariantOptionData(productModel, variantOptionData);
-        matrixElementData.setVariantOption(variantOptionData);
-
+        VariantMatrixElementData sizeElementData = createNode(sizeVariantProductModel);
+        sizeElementData.setIsLeaf(Boolean.TRUE);
+        styleElementData.getElements().add(sizeElementData);
       }
     }
 
-
-
+    productData.setVariantMatrix(rootElementData.getElements());
   }
 
   protected ProductModel getBaseProduct(ProductModel productModel) {
@@ -57,6 +53,15 @@ public class PentlandProductVariantMatrixPopulator<SOURCE extends ProductModel, 
       return productModel;
     }
     return productModel;
+  }
+
+  protected VariantMatrixElementData createNode(ProductModel productModel) {
+    VariantMatrixElementData elementData = new VariantMatrixElementData();
+    VariantOptionData variantOptionData = newVariantOptionData();
+    populateVariantOptionData(productModel, variantOptionData);
+    elementData.setVariantOption(variantOptionData);
+    elementData.setElements(new ArrayList<>());
+    return elementData;
   }
 
   protected void populateVariantOptionData(ProductModel productModel, VariantOptionData variantOptionData) {
