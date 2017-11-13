8e764eb192d76468990742f35bc7fb18387f9553
Kate Agievich <ekaterina.agievich@masterdata.ru>
2017-11-13 14:54:10 +0300

    #BRIDGEX-47 fixed repeated addition of 0 quantity to cart

--- hybris/bin/custom/pentland/pentlandcore/src/com/bridgex/core/order/impl/PentlandCartServiceForAccelerator.java
+++ hybris/bin/custom/pentland/pentlandcore/src/com/bridgex/core/order/impl/PentlandCartServiceForAccelerator.java
@@ -1,6 +1,12 @@
 package com.bridgex.core.order.impl;
 
+import java.util.*;
+
+import org.apache.commons.collections.MapUtils;
+
 import de.hybris.platform.acceleratorservices.order.impl.DefaultCartServiceForAccelerator;
+import de.hybris.platform.core.model.order.CartEntryModel;
+import de.hybris.platform.core.model.order.CartModel;
 
 /**
  * @author Created by ekaterina.agievich@bridge-x.com on 11/8/2017.
@@ -17,6 +23,51 @@ public class PentlandCartServiceForAccelerator extends DefaultCartServiceForAcce
     if (number < APPEND_AS_LAST) {
       throw new IllegalArgumentException("Number must be greater or equal -1");
     }
+  }
+
+  @Override
+  public void updateQuantities(final CartModel cart, final Map<Integer, Long> quantities)
+  {
+    if (!MapUtils.isEmpty(quantities)) {
+      final Collection<CartEntryModel> toRemove = new LinkedList<CartEntryModel>();
+      final Collection<CartEntryModel> toSave = new LinkedList<CartEntryModel>();
+      for (final Map.Entry<CartEntryModel, Long> e : getEntryQuantityMap(cart, quantities).entrySet()) {
+        final CartEntryModel cartEntry = e.getKey();
+        final Long quantity = e.getValue();
+        if (quantity == null || quantity < 0) {
+          toRemove.add(cartEntry);
+        }else {
+          cartEntry.setQuantity(quantity);
+          toSave.add(cartEntry);
+        }
+      }
+      getModelService().removeAll(toRemove);
+      getModelService().saveAll(toSave);
+      getModelService().refresh(cart);
+    }
+  }
+
+  private Map<CartEntryModel, Long> getEntryQuantityMap(final CartModel cart, final Map<Integer, Long> quantities) {
+    final List<CartEntryModel> entries = (List)cart.getEntries();
+
+    final Map<CartEntryModel, Long> ret = new LinkedHashMap<CartEntryModel, Long>();
+
+    for (final Map.Entry<Integer, Long> q : quantities.entrySet()) {
+      final Integer entryNumber = q.getKey();
+      final Long quantity = q.getValue();
+      ret.put(getEntry(entries, entryNumber), quantity);
+    }
+
+    return ret;
+  }
+
+  private CartEntryModel getEntry(final List<CartEntryModel> entries, final Integer entryNumber) {
+    for (final CartEntryModel e : entries) {
+      if (entryNumber.equals(e.getEntryNumber())) {
+        return e;
+      }
+    }
+    throw new IllegalArgumentException("no cart entry found with entry number " + entryNumber + " (got " + entries + ")");
   }
 
 }
