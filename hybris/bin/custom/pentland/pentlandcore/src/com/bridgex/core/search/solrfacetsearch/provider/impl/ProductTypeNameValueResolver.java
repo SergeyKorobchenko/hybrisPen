569855066dc6f033a825e3e64960ebe7d0851fb2
Kate Agievich <ekaterina.agievich@masterdata.ru>
2017-11-28 12:32:12 +0300

    #BRIDGEX-48 changed category facets

---
+++ hybris/bin/custom/pentland/pentlandcore/src/com/bridgex/core/search/solrfacetsearch/provider/impl/ProductTypeNameValueResolver.java
@@ -0,0 +1,20 @@
+package com.bridgex.core.search.solrfacetsearch.provider.impl;
+
+import de.hybris.platform.category.model.CategoryModel;
+import de.hybris.platform.solrfacetsearch.config.IndexedProperty;
+import de.hybris.platform.solrfacetsearch.config.exceptions.FieldValueProviderException;
+import de.hybris.platform.solrfacetsearch.indexer.spi.InputDocument;
+
+/**
+ * @author Created by ekaterina.agievich@bridge-x.com on 10/23/2017.
+ */
+public class ProductTypeNameValueResolver extends AbstractProductTypeResolver {
+
+  protected void addFieldValue(InputDocument inputDocument, IndexedProperty indexedProperty, ValueResolverContext<Object, Object> valueResolverContext, CategoryModel cat)
+    throws FieldValueProviderException {
+
+    inputDocument.addField(indexedProperty, cat.getName(), valueResolverContext.getFieldQualifier());
+
+  }
+
+}
