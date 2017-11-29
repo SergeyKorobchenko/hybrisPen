8772f7423d96c9397dab32886e6a792d6301ac1a
Kate Agievich <ekaterina.agievich@masterdata.ru>
2017-11-29 15:57:43 +0300

    fixed cart entry base price calculation

--- hybris/bin/custom/pentland/pentlandcore/src/com/bridgex/core/order/impl/DefaultPentlandCalculationService.java
+++ hybris/bin/custom/pentland/pentlandcore/src/com/bridgex/core/order/impl/DefaultPentlandCalculationService.java
@@ -2,12 +2,16 @@ package com.bridgex.core.order.impl;
 
 import java.util.Collection;
 import java.util.Date;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Required;
 
 import de.hybris.platform.core.model.order.AbstractOrderEntryModel;
 import de.hybris.platform.core.model.order.AbstractOrderModel;
 import de.hybris.platform.core.model.order.CartModel;
 import de.hybris.platform.order.exceptions.CalculationException;
 import de.hybris.platform.order.impl.DefaultCalculationService;
+import de.hybris.platform.order.strategies.calculation.OrderRequiresCalculationStrategy;
 import de.hybris.platform.util.PriceValue;
 import de.hybris.platform.util.TaxValue;
 
@@ -15,6 +19,9 @@ import de.hybris.platform.util.TaxValue;
  * @author Created by konstantin.pavlyukov on 11/10/2017.
  */
 public class DefaultPentlandCalculationService extends DefaultCalculationService {
+
+  private OrderRequiresCalculationStrategy pentlandOrderRequiresCalculationStrategy;
+
   protected void resetAllValues(final AbstractOrderEntryModel entry) throws CalculationException
   {
     // taxes
@@ -32,15 +39,24 @@ public class DefaultPentlandCalculationService extends DefaultCalculationService
   }
 
   @Override
-  public void calculate(final AbstractOrderModel order) throws CalculationException
-  {
-    //disable calculation
+  public void calculate(final AbstractOrderModel order) throws CalculationException {
+    //disable calculation, reset base price only
+    if (pentlandOrderRequiresCalculationStrategy.requiresCalculation(order)) {
+      calculateEntries(order, false);
+    }
   }
 
   @Override
   public void calculate(final AbstractOrderModel order, final Date date) throws CalculationException
   {
-    //disable calculation
+    final Date old = order.getDate();
+    order.setDate(date);
+    try {
+      this.calculate(order);
+    }finally {
+      order.setDate(old);
+      getModelService().save(order);
+    }
   }
 
   @Override
@@ -49,4 +65,15 @@ public class DefaultPentlandCalculationService extends DefaultCalculationService
     //disable calculation
   }
 
+  protected void recalculateOrderEntryIfNeeded(final AbstractOrderEntryModel entry, final boolean forceRecalculation)
+    throws CalculationException {
+    if (forceRecalculation || pentlandOrderRequiresCalculationStrategy.requiresCalculation(entry)) {
+      resetAllValues(entry);
+    }
+  }
+
+  @Required
+  public void setPentlandOrderRequiresCalculationStrategy(OrderRequiresCalculationStrategy pentlandOrderRequiresCalculationStrategy) {
+    this.pentlandOrderRequiresCalculationStrategy = pentlandOrderRequiresCalculationStrategy;
+  }
 }
