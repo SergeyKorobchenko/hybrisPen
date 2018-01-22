b6e720801c746a1d009ed8e728b722bc1bd919b0
Kate Agievich <ekaterina.agievich@masterdata.ru>
2018-01-22 11:49:04 +0300

    #CEPB2B-631 fixed unsaved cart evaluation

--- hybris/bin/custom/pentland/pentlandstorefront/web/webroot/_ui/responsive/common/js/acc.cart.js
+++ hybris/bin/custom/pentland/pentlandstorefront/web/webroot/_ui/responsive/common/js/acc.cart.js
@@ -502,7 +502,10 @@ ACC.cart = {
     },
 
     bindScanCartForEdits: function() {
-        $(".js-continue-checkout-button, .export__cart--link, .export__images--link, .js-create-quote-button, .js-continue-shopping-button").on("click", ACC.cart.scanCartForEdits);
+        if ($('#updateAllCartForm').length > 0 && 'CART' === $('#updateAllCartForm').data('pageType')) {
+            //don't do this on document.ready, it'll cause event to propagate further even if the condition in this method worked
+            $(".js-continue-checkout-button, .export__cart--link, .export__images--link, .js-create-quote-button, .js-continue-shopping-button").on("click", ACC.cart.scanCartForEdits);
+        }
         $(document).ready(function () {
             if ($('#updateAllCartForm').length > 0 && 'CART' === $('#updateAllCartForm').data('pageType')) {
                 $("a[href], .js_search_button").on("click", ACC.cart.scanCartForEdits);
