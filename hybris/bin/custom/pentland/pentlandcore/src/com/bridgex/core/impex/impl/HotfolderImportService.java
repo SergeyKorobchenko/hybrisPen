5b0fa1fbf3d582ff9c1ceffb45f922dcd7c63fb6
Kate Agievich <ekaterina.agievich@masterdata.ru>
2018-01-22 13:42:31 +0300

    removed binding of hot folder import to current node

--- hybris/bin/custom/pentland/pentlandcore/src/com/bridgex/core/impex/impl/HotfolderImportService.java
+++ hybris/bin/custom/pentland/pentlandcore/src/com/bridgex/core/impex/impl/HotfolderImportService.java
@@ -40,11 +40,11 @@ public class HotfolderImportService extends DefaultImportService {
     } catch (ModelInitializationException var4) {
       throw new SystemException(var4);
     }
-    LOG.info("Created cronjob for hot folder import " + cronJob.getCode());
+    LOG.info("Created cronjob for hot folder import, configuring... ");
     this.configureCronJob(cronJob, config);
-    cronJob.setNodeID(clusterService.getClusterId());
+
     this.getModelService().saveAll(cronJob.getJob(), cronJob);
-    LOG.info("Succesfully configured job " + cronJob.getCode() + " to import on cluster node " + cronJob.getNodeID());
+    LOG.info("Succesfully configured job " + cronJob.getCode());
     this.importData(cronJob, config.isSynchronous(), config.isRemoveOnSuccess());
     return ((Item)this.getModelService().getSource(cronJob)).isAlive() ? new ImportCronJobResult(cronJob) : new ImportCronJobResult(null);
   }
