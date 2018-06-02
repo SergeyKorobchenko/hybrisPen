654f84d3a3e980c48c98c9575b608b39111f514f
Murali <murali.venkata@bridge-x.com>
2018-06-02 22:13:47 +0530

    committing changes for connect-104

--- hybris/bin/custom/pentland/pentlandstorefront/web/src/com/bridgex/storefront/controllers/pages/LoginPageController.java
+++ hybris/bin/custom/pentland/pentlandstorefront/web/src/com/bridgex/storefront/controllers/pages/LoginPageController.java
@@ -10,6 +10,10 @@
  */
 package com.bridgex.storefront.controllers.pages;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,6 +26,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Validator;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -31,11 +36,15 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.bridgex.facades.customer.PentlandCustomerRegFacade;
 import com.bridgex.storefront.controllers.ControllerConstants;
 
+import de.hybris.platform.acceleratorstorefrontcommons.breadcrumb.Breadcrumb;
 import de.hybris.platform.acceleratorstorefrontcommons.controllers.pages.AbstractLoginPageController;
+import de.hybris.platform.acceleratorstorefrontcommons.controllers.util.GlobalMessages;
 import de.hybris.platform.acceleratorstorefrontcommons.forms.RegisterForm;
 import de.hybris.platform.cms2.exceptions.CMSItemNotFoundException;
 import de.hybris.platform.cms2.model.pages.AbstractPageModel;
+import de.hybris.platform.cms2.model.pages.ContentPageModel;
 import de.hybris.platform.commercefacades.user.data.RegisterData;
+import de.hybris.platform.commercefacades.user.data.TitleData;
 import de.hybris.platform.commerceservices.customer.DuplicateUidException;
 
 
@@ -47,6 +56,8 @@ import de.hybris.platform.commerceservices.customer.DuplicateUidException;
 public class LoginPageController extends AbstractLoginPageController
 {
 	 private static final Logger LOG = Logger.getLogger(LoginPageController.class);
+	 public final static String CMS_REGISTER_ACCESS_PAGE_NAME = "SecureCustomerAccessRegisterPage";
+	 public final static String CMS_REGISTER_ACCESS_SUCCESS_PAGE_NAME = "SecureCustomerAccessRegisterSuccessPage";
 	private HttpSessionRequestCache httpSessionRequestCache;
 	
 	@Resource(name="pentlandCustomerRegFacade")
@@ -55,6 +66,7 @@ public class LoginPageController extends AbstractLoginPageController
 	 @Resource(name = "pentlandRegistrationValidator")
 	 private Validator pentlandRegistrationValidator;
 
+		 
 	@Override
 	protected String getView()
 	{
@@ -116,23 +128,68 @@ public class LoginPageController extends AbstractLoginPageController
 	}
 	
 	@RequestMapping(value = "/registerForAccess", method = RequestMethod.GET)
-	 public String getRegisterPage(final Model model) throws CMSItemNotFoundException {
-
+	 public String getRegisterPage(final Model model) throws CMSItemNotFoundException
+	{
 	  LOG.info("at registerForAccess.. method");
-	  PentlandCustomerRegistrationForm pentlandCustomerRegistrationForm = new PentlandCustomerRegistrationForm();
-	  model.addAttribute("pentlandCustomerRegistrationForm", pentlandCustomerRegistrationForm);
-	  return ControllerConstants.Views.Pages.Footer.RegisterforAccessPage;
+	  return getDefaultRegistrationPage(model, getContentPageForLabelOrId(getRegistrationCmsPage()));
 	 }
+	
+	protected String getRegistrationCmsPage()
+	{
+		return CMS_REGISTER_ACCESS_PAGE_NAME;
+	}
+	
+	protected String getRegisterAccessSuccessCmsPage()
+	{
+		return CMS_REGISTER_ACCESS_SUCCESS_PAGE_NAME;
+	}
 
+	protected String getRegistrationView()
+	{
+		return ControllerConstants.Views.Pages.Footer.RegisterforAccessPage;
+	}
+	
+	protected String getRegisterAccessSuccessView()
+	{
+		return ControllerConstants.Views.Pages.Footer.SuccessRegister;
+	}
+	
+	protected String getDefaultRegistrationPage(final Model model, final ContentPageModel contentPageModel)
+	{
+		populateModelCmsContent(model, contentPageModel);
+		model.addAttribute("pentlandCustomerRegistrationForm", new PentlandCustomerRegistrationForm());
+		return getRegistrationView();
+	}
+	
+	protected String getDefaultRegisterAccessSuccessPage(final Model model, final ContentPageModel contentPageModel)
+	{
+		final Breadcrumb registerSuccessBreadcrumbEntry = new Breadcrumb("#",
+				getMessageSource().getMessage("breadcrumb.register.access.success", null, getI18nService().getCurrentLocale()), null);
+		model.addAttribute("breadcrumbs", Collections.singletonList(registerSuccessBreadcrumbEntry));
+		storeCmsPageInModel(model, contentPageModel);
+		setUpMetaDataForContentPage(model, contentPageModel);
+		return getRegisterAccessSuccessView();
+	}
+	
+	protected void populateModelCmsContent(final Model model, final ContentPageModel contentPageModel)
+	{
+		storeCmsPageInModel(model, contentPageModel);
+		setUpMetaDataForContentPage(model, contentPageModel);
+
+		final Breadcrumb registrationBreadcrumbEntry = new Breadcrumb("#",
+				getMessageSource().getMessage("breadcrumb.register.access", null, getI18nService().getCurrentLocale()), null);
+		model.addAttribute("breadcrumbs", Collections.singletonList(registrationBreadcrumbEntry));
+	}
+	
 	 @RequestMapping(value = "/registered", method = RequestMethod.POST)
 	 public String postFormData(PentlandCustomerRegistrationForm pentlandCustomerRegistrationForm, final Model model,
-	   final BindingResult bindingResult, final HttpServletRequest request, final HttpServletResponse response) {
+	   final BindingResult bindingResult, final HttpServletRequest request, final HttpServletResponse response) throws CMSItemNotFoundException {
 	  LOG.info("at registered.. method");
 	  pentlandRegistrationValidator.validate(pentlandCustomerRegistrationForm, bindingResult);
 	  if (bindingResult.hasErrors()) {
 	   model.addAttribute(pentlandCustomerRegistrationForm);
 	   // GlobalMessages.addErrorMessage(model, FORM_GLOBAL_ERROR);
-	   return ControllerConstants.Views.Pages.Footer.RegisterforAccessPage;
+	   return getDefaultRegistrationPage(model, getContentPageForLabelOrId(getRegistrationCmsPage()));
 	  }
 	  try {
 
@@ -151,12 +208,14 @@ public class LoginPageController extends AbstractLoginPageController
 	    pentlandCustomerRegFacade.saveFormData(data);
 
 	   }
-	   return ControllerConstants.Views.Pages.Footer.SuccessRegister;
+	   return getDefaultRegisterAccessSuccessPage(model, getContentPageForLabelOrId(getRegisterAccessSuccessCmsPage()));
 
 	  } catch (DuplicateUidException e) {
 
 	   LOG.error("fail to register:"+e);
-	   return ControllerConstants.Views.Pages.Footer.FailRegister;
+	   GlobalMessages.addMessage(model, GlobalMessages.ERROR_MESSAGES_HOLDER, "register.access.dublicate.mail", new Object[]
+				{pentlandCustomerRegistrationForm.getEmail()});
+	   return getDefaultRegistrationPage(model, getContentPageForLabelOrId(getRegistrationCmsPage()));
 	  }
 
 	 }
--- hybris/bin/custom/pentland/pentlandstorefront/web/webroot/WEB-INF/views/responsive/pages/footer/successPage.jsp
+++ hybris/bin/custom/pentland/pentlandstorefront/web/webroot/WEB-INF/views/responsive/pages/footer/successPage.jsp
@@ -1,12 +1,12 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
-</head>
-<body>
-    <h3>Successfully submitted</h3>
-</body>
-</html>
+<%@ page trimDirectiveWhitespaces="true"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="template" tagdir="/WEB-INF/tags/responsive/template"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%> 
+
+<spring:htmlEscape defaultHtmlEscape="true" />
+<template:page pageTitle="${pageTitle}">
+	<jsp:body>
+       <h3>Successfully submitted</h3>
+	</jsp:body>
+</template:page>
