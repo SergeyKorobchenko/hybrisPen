12d6fa3097d2c344891e5fa1df68869baf290577
Artem Zur <artem.zur@masterdata.ru>
2018-08-01 14:18:29 +0300

    Refactored spring xml

--- hybris/bin/custom/pentland/pentlandb2cstorefront/web/src/com/bridgex/b2cstorefront/filters/RedirectToCartFilter.java
+++ hybris/bin/custom/pentland/pentlandb2cstorefront/web/src/com/bridgex/b2cstorefront/filters/RedirectToCartFilter.java
@@ -7,20 +7,32 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 
 public class RedirectToCartFilter extends OncePerRequestFilter {
 
-    private static final List<String> URLS = Arrays.asList(new String[]{"/cart", "/login/checkout", "/login/pw/change"});
-    private static final String MULTI_URL = "/checkout/";
+    private List<String> URLS;
+    private String MULTI_URL;
+    private String CART_URL;
 
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
 
         if(!(URLS.contains(httpServletRequest.getServletPath()) || httpServletRequest.getServletPath().startsWith(MULTI_URL))){
-            httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + "/cart");
+            httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + CART_URL);
         }
         filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+
+    public void setURLS(List<String> URLS) {
+        this.URLS = URLS;
+    }
+
+    public void setMULTI_URL(String MULTI_URL) {
+        this.MULTI_URL = MULTI_URL;
+    }
+
+    public void setCART_URL(String CART_URL) {
+        this.CART_URL = CART_URL;
     }
 }
